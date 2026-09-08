5be5fde9b9b4fb62e3c72e4ce2966973178f6238
Emanuel Flores <emanuel.flores@vallegrande.edu.pe>
2026-09-07 22:40:52 -0500

    Basic comment update

--- Manejo-de-archivos/app/static/js/style.js
+++ Manejo-de-archivos/app/static/js/style.js
@@ -1,3 +1,4 @@
+// Cuando el documento HTML termine de cargarse, ejecuta lo siguiente
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Sistema de Manejo de Archivos y Datos listo.");
 });
